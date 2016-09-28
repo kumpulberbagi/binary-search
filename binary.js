8485957b4fc8e12df376c297bc553f94b81a4a99
Aji Lantang Mardika & Juang Wiantoro <pair+ajilantang.broerjuang@hacktiv8.com>
2016-09-28 09:07:46 +0700

    submit of binary search

--- binary.js
+++ binary.js
@@ -20,25 +20,20 @@
 var test_array_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var test_array_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-function binary_search(search, array) {
-  var mid = Math.floor(array.length/2)
-  if (search === array[mid]) {
-    return mid
-  }else if (search > array[mid]) {
-    for (var i = mid+1; i < array.length; i++) {
-      if (search === array[i]) {
-        return i
-      }
-    }
-  }else if (search < array[mid]) {
-    for (var i = 0; i < mid; i++) {
-      if (search === array [i]) {
-        return i
-      }
+function binary_search(target, array) {
+  var min = 0, max = array.length - 1;
+  var guess;
+  while (min <= max) {
+    guess = Math.floor((min + max) / 2);
+    if (array[guess] === target) {
+      return guess;
+    } else if (array[guess] < target) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
     }
   }
-  return -1
-  // Your code here
+  return -1;
 }
 
 // Driver code
