3bea0070b6f06cc842d2abb2b07610083a5a6aba
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-16 15:09:38 +0700

    fix bug return -1 jika tidak dapat jawaban

--- binary.js
+++ binary.js
@@ -9,46 +9,51 @@
 // ok the array is our data set which we will be comparing our object against to find the index position
 // ok establish the index position of the element in our array which is the mid-way point in our array (probably by using array.length)
 // make sure that this works for both even and odd array lengths
-//create a method to cut our array length in half, and determine if our object is higher or lower than the array number at that "half array" index position
-// if our number is exactly the same as the "half array" index number, return the index position of that number in the array.
+// create a method to cut our array length in half, and determine if our object is higher or lower than the array number at that 'half array' index position
+// if our number is exactly the same as the 'half array' index number, return the index position of that number in the array.
 // if our number is higher, discard the lower half of our array set and continue testing against the upper half of our array (taking the middle position each time).
 // if lower, do the opposite of above.
-//continue until "half_array" index is equal to number, at which point return the index position of number chosen.
+// continue until 'half_array' index is equal to number, at which point return the index position of number chosen.
 
-"use strict"
+'use strict'
 
 var test_array_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var test_array_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-var test_array_c = [100,120,130,135,150,170]
-var test_array_d = [13,19,24,29,32,37,43]
+var test_array_c = [100, 120, 130, 135, 150, 170]
+var test_array_d = [13, 19, 24, 29, 32, 37, 43]
 
-function binary_search(search, array) {
+function binary_search (search, array) {
   // Your code here
   var found = false
   var first = 0
-  var last = array.length-1
-  var mid = Math.floor((first+last)/2)-1
+  var last = array.length - 1
+  var mid = Math.floor((first + last) / 2) - 1
 
-  while(found == false){
-      if(search === array[mid]){
+  while (found === false) {
+    if (search < array[first] || search > array[last]) {
+      mid = -1
       found = true
-    } else if (search < array[mid]){
-      last = mid
-      mid = Math.floor((first+last)/2)
-    } else if(search > array[mid]) {
-      first = mid + 1
-      mid = Math.floor((first+last)/2)
+    } else {
+      if (search === array[mid]) {
+        found = true
+      } else if (search < array[mid]) {
+        last = mid
+        mid = Math.floor((first + last) / 2)
+      } else if (search > array[mid]) {
+        first = mid + 1
+        mid = Math.floor((first + last) / 2)
+      }
     }
   }
   return mid
 }
 
 // Driver code
-console.log("4  | " + binary_search(5, test_array_a))
-console.log("5  | " + binary_search(6, test_array_b))
-console.log("9  | " + binary_search(10, test_array_a))
-console.log("10 | " + binary_search(11, test_array_b))
-console.log("1  | " + binary_search(2, test_array_a))
-console.log("1  | " + binary_search(2, test_array_b))
-console.log("3  | " + binary_search(135, test_array_c))
-// console.log("-1  | " + binary_search(35, test_array_d)) masih error
+console.log('jawaban : 4  | hasil : ' + binary_search(5, test_array_a))
+console.log('jawaban : 5  | hasil : ' + binary_search(6, test_array_b))
+console.log('jawaban : 9  | hasil : ' + binary_search(10, test_array_a))
+console.log('jawaban : 10 | hasil : ' + binary_search(11, test_array_b))
+console.log('jawaban : 1  | hasil : ' + binary_search(2, test_array_a))
+console.log('jawaban : 1  | hasil : ' + binary_search(2, test_array_b))
+console.log('jawaban : -1  | hasil : ' + binary_search(200, test_array_c))
+console.log('jawaban : -1  | hasil : ' + binary_search(35, test_array_d))
